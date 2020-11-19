5240d9d7c707935bd2f35d7a6000ed07097c75bf
manaliRedkar <msredkar28@gmail.com>
2020-11-19 01:22:26 -0800

    Initil merge website with AR

---
+++ Quick.js
@@ -0,0 +1,21 @@
+var person = window.prompt("Please enter your name:");
+if (person != null) 
+	txt =  person + ", Welcome to Quick Link!";
+document.write(txt.bold());
+
+var customer = 0;
+var Walmart[10];
+var Trader[10];
+var Safeway[10];
+
+
+function count()
+
+{
+ var shop = document.getElementById("shoptype");
+ var selectedValue = shop.options[ddl.selectedIndex].value;
+    if (selectedValue == "walmart")
+   {
+    alert("you are at");
+   }
+}
